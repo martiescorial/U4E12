4a5415b01ae7b9d9fa9ce72704ab34f83bbbfe7e
Martina <martinaescorial9@gmail.com>
2021-06-19 14:36:24 +0200

    Add project files.

---
+++ U4E12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace U4E12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Martina";
+            Console.WriteLine("Bienvenida " + nombre);
+        }
+    }
+}
